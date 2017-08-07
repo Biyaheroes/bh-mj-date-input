ea72ff49ccb02437caa97976476583902e6f7f7f
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-07 12:34:31 +0800

    Update bh-mj-date-input component.

---
+++ component.jsx
@@ -0,0 +1,15 @@
+
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import DateInput from "./date-input.module.jsx";
+
+ReactDOM.render( 
+		<DateInput
+		label="This is a date input">
+		</DateInput>,
+		
+	document.getElementById( "root" ) 
+);
--- test.jsx
+++
@@ -1,9 +0,0 @@
-
-"use strict";
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import DateInput from "./date-input.js";
-
-ReactDOM.render( <DateInput></DateInput> );
